5f49d6a983efe3b6c818e14cf397d14ed4fe2cbd
michelleitaly <michelle22289@gmail.com>
2022-04-06 21:24:08 +0200

    1

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+let cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -27,6 +27,12 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+let listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//my code:
+  let jsonObject= {};
+app.get("/api/:input", (req, res)=>{
+    res.json(jsonObject);
+}) 
