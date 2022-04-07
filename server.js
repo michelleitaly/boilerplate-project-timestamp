95cd876133a86db13a15ed3fb44beab80e589476
michelleitaly <michelle22289@gmail.com>
2022-04-07 13:23:42 +0200

    04072022

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ let app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 let cors = require('cors');
+const parse = require('nodemon/lib/cli/parse');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -34,5 +35,22 @@ let listener = app.listen(process.env.PORT, function () {
 //my code:
   let jsonObject= {};
 app.get("/api/:input", (req, res)=>{
+  let input = req.params.input;
+  if(input.includes("-")){
+    jsonObject["unix"] = new Date(input).getTime();
+     jsonObject["utc"] = new Date(input).toUTCString();
+    }else{
+    input = parseInt(input)  
+    jsonObject["unix"] = new Date(input).getTime();
+    jsonObject["utc"] = new Date(input).toUTCString(); 
+    }
+    if(!jsonObject["unix"] || jsonObject["utc"]){
+      console.log({error: "invalid Date"})
+    }
     res.json(jsonObject);
 }) 
+app.get("/api", (req, res)=>{
+  jsonObject["unix"]= new Date().getTime();
+  jsonObject["utc"] = new Date().toUTCString();
+  res.json(jsonObject)
+})
